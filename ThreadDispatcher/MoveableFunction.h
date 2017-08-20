9608f0582f5053a92420a7cd5ae9c26c3cb9fd7c
Vladimir Krasnotsvetov <vvkraf@gmail.com>
2017-08-20 20:07:58 +0300

    Implement moveable function.

---
+++ ThreadDispatcher/MoveableFunction.h
@@ -0,0 +1,45 @@
+#pragma once
+#ifndef MOVEABLE_FUNCTION
+#define MOVEABLE_FUNCTION
+
+#include <memory>
+
+namespace Dispatcher {
+	namespace Utils {
+
+		template <typename FunctionType>
+		class MoveableFunction;
+
+		template <typename ReturnType, typename ... ArgumentTypes>
+		class MoveableFunction<ReturnType(ArgumentTypes...)> {
+		public:
+			MoveableFunction() = delete;
+			template <typename Function>
+			MoveableFunction(Function function) : myInvoker(new Invoker<Function>(std::move(function))) {}
+			ReturnType operator()(ArgumentTypes... args) {
+				return myInvoker->Invoke(args...);
+			}
+
+		private:
+			class InvokerBase {
+			public:
+				InvokerBase() {}
+				virtual ReturnType Invoke(ArgumentTypes... args) = 0;
+				virtual ~InvokerBase() {};
+			};
+
+			template<typename Function>
+			class Invoker : public InvokerBase {
+			public:
+				Invoker(Function&& func) : InvokerBase(), function(std::move(func)) {}				virtual ReturnType Invoke(ArgumentTypes... args)				{
+					return function(args ...);
+				}
+			private:
+				Function function;
+			};
+
+			std::unique_ptr<InvokerBase> myInvoker;
+		};
+#endif // !MOVEABLE_FUNCTION
+	}
+}
--- ThreadDispatcher/Program.cpp
+++ ThreadDispatcher/Program.cpp
@@ -1,9 +1,24 @@
 #pragma once
 #include "ThreadDispatcher.h"
 using namespace Dispatcher;
+#define TEST
+#ifdef TEST
+#include <functional>
+#include <future>
+#include "MoveableFunction.h"
+#endif // TEMP
+
+
 
 int main() {
 	ThreadDispatcher dispatcher(5);
 	auto t = dispatcher.AddTask([](int a) {return  ++a; }, 1);
 	auto result = t.get();
+
+	int x = 5;
+	std::promise<void> promise;
+	//don't work
+	//std::function<void()> testVoid([p = std::move(promise)]() {});
+	//work
+	Dispatcher::Utils::MoveableFunction<void()> zzz([p = std::move(promise)]() {});
 }
